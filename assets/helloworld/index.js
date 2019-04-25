d7309ee85984d6ea86e2967ca2073d87bf846da9
Weijia Wang <starkwang@126.com>
2019-04-25 11:31:04 +0800

    change helloworld port

--- assets/helloworld/index.js
+++ assets/helloworld/index.js
@@ -3,4 +3,4 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(5000, () => console.log('Example app listening on port 5000!'))
