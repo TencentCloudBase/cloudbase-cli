8f886e887b8d880af22cfa1101c609f9396256a1
dongyuanxin <yuanxin.me@gmail.com>
2019-04-29 19:25:21 +0800

    add deploy test

---
+++ test/deploy/function.spec.ts
@@ -0,0 +1,19 @@
+import FunctionDeploy from '../../src/deploy/function'
+import { iFunctionDeployConfig } from '../config'
+
+describe('class FunctionDeploy', () => {
+    
+    test('#deploy', async () => {
+        expect.assertions(1)
+
+        const deployer = new FunctionDeploy(iFunctionDeployConfig)
+        try {
+            const result = await deployer.deploy()
+            expect(result).toBeUndefined()
+        } catch (error) {
+            console.error(error)
+            expect(typeof error).toBe('object')
+        }
+    })
+
+})
---
+++ test/deploy/node.spec.ts
@@ -0,0 +1,32 @@
+import NodeDeploy from '../../src/deploy/node'
+import { iNodeDeployConfig } from '../config'
+
+describe('class NodeDeploy', () => {
+    let deployer: NodeDeploy | null = null
+
+    beforeAll(async () => {
+        deployer = new NodeDeploy(iNodeDeployConfig)
+    })
+
+    afterAll(async () => {
+        deployer = null
+    })
+    
+    test('#deploy', async () => {
+        expect.assertions(1)
+
+        try {
+            const result = await deployer.deploy()
+            expect(result).toBeUndefined()
+        } catch (error) {
+            console.error(error)
+            expect(typeof error).toBe('object')
+        }
+    })
+
+    test('#clear', () => {
+        const result = deployer.clear()
+        expect(result).toBeUndefined()
+    })
+
+})
