4fc60f481813e4b707587799b6ef4e93c31214e5
dongyuanxin <yuanxin.me@gmail.com>
2019-04-29 19:25:07 +0800

    add controller test

---
+++ test/controller/node.spec.ts
@@ -0,0 +1,63 @@
+import NodeController from '../../src/controller/node'
+import { iNodeDeployConfig } from '../config'
+
+describe('class NodeController', () => {
+    let controller: NodeController | null = null
+
+    beforeAll(async () => {
+        controller = new NodeController(iNodeDeployConfig)
+    })
+
+    afterAll(async () => {
+        controller = null
+    })
+
+    test('#start', async () => {
+        expect.assertions(1)
+
+        try {
+            const result = await controller.start({ vemo: true })
+            expect(result).toBeUndefined()
+        } catch (error) {
+            console.error(error)
+            expect(typeof error).toBe('object')
+        }
+    })
+
+    test('#logs', async () => {
+        expect.assertions(1)
+
+        try {
+            const result = await controller.logs({ lines: 20 })
+            expect(result).toBeUndefined()
+        } catch (error) {
+            console.error(error)
+            expect(typeof error).toBe('object')
+        }
+    })
+
+    test('#show', async () => {
+        expect.assertions(1)
+
+        try {
+            const result = await controller.show()
+            expect(result).toBeUndefined()
+        } catch (error) {
+            console.error(error)
+            expect(typeof error).toBe('object')
+        }
+    })
+
+    test('#delete', async () => {
+        expect.assertions(1)
+
+        try {
+            const result = await controller.delete()
+            expect(result).toBeUndefined()
+        } catch (error) {
+            console.error(error)
+            expect(typeof error).toBe('object')
+        }
+    })
+
+})
