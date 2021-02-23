57be538e0bd0c70364b349af58c25c981ab4bf58
cwuyiqing <cwuyiqing@gmail.com>
2021-02-23 19:26:47 +0800

    doc: add globalThis hack comment

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,8 @@ export { smartDeploy } from './commands'
 
 registerCommands()
 
+// HACK: 部分依赖使用了 globalThis，会在 Node12 以下的版本报错
 if (typeof globalThis === undefined) {
-  // eslint-disable-next-line no-undef
-  (globalThis as any) = global
+    // eslint-disable-next-line no-undef
+    ;(globalThis as any) = global
 }
