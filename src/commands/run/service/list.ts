8e82ac8c0156415dd854802ba57a2c9532f26a38
yoelali <yoelali@tencent.com>
2022-05-20 16:15:06 +0800

    fix: 修正无服务时 JSON 输出信息

--- src/commands/run/service/list.ts
+++ src/commands/run/service/list.ts
@@ -52,26 +52,26 @@ export class ListServiceTcbr extends Command {
             envId: envId
         })
         loading.stop()
-
-        if (!serverList.length) {
-            console.log('当前环境下没有服务')
-            return
-        }
-
+        
         const specificServer = serverList.filter(serverItem => serverItem.ServerName === serviceName)
-
+        
         // 打印 JSON 形式结果
         if (options.json) {
             console.log(JSON.stringify({
                 code: 0,
                 errmsg: 'success',
                 data: specificServer.length ?
-                    specificServer
-                    : serverList
+                specificServer
+                : serverList
             }, null, 2))
             return
         }
-
+        
+        if (!serverList.length) {
+            console.log('当前环境下没有服务')
+            return
+        }
+        
         // 如有指定服务名，则只打印指定服务的信息
         let tableData: (string | number)[][]
         if (specificServer.length) {
