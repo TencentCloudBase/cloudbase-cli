b48fb60a638d7fc32affce8b746cce3e06bdfbf4
Daniel.xiao <dxweixiao@tencent.com>
2024-04-09 20:05:44 +0800

    chore: upgrade @cloudbase/addon-cli

--- src/commands/addon/index.ts
+++ src/commands/addon/index.ts
@@ -100,7 +100,7 @@ function getOptions({
         options: [
             {
                 flags: '--resource <resource>',
-                desc: '资源名称。当想操作指定资源而非插件时使用。当前支持 automation'
+                desc: '资源名称。当想操作指定资源而非插件时使用。当前支持 automation / block'
             },
             ...options
         ],
